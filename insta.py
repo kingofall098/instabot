a05f5d29bd150c5591f65feb7c57d0f07cd00a9b
kingofall098 <joewad@indogmail.com>
2026-03-12 20:28:18 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -14,7 +14,7 @@ import time
 # BOT CONFIG
 # =====================================
 
-TOKEN = "8628280617:AAEHHRQZ2dxsxoFWvmLs1PVO_wSCRn0rHPc"
+TOKEN = "8756448611:AAHbnOlBbZP8639ZKHcFZd0vSQeK54EMSYQ
 
 bot = telebot.TeleBot(TOKEN)
 
