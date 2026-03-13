cbfe970ac9b4506a2dbc77daa2a5d5d4890b1d92
kingofall098 <joewad@indogmail.com>
2026-03-14 05:14:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
+#!/bin/bash
+
 apt-get update
 apt-get install -y xvfb xauth
 
 pip install -r requirements.txt
+
 playwright install --with-deps chromium
-
-xvfb-run python insta.pyapt-get update
-apt-get install -y xvfb
-
-playwright install chromium
 
 xvfb-run python insta.py
