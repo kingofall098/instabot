3f7b52ab3670e9fce5ab9499cd5ade9f2626aac3
kingofall098 <joewad@indogmail.com>
2026-03-15 02:34:24 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -10,7 +10,7 @@ import re
 import uuid
 import time
 import random
-8756448611:AAHbnOlBbZP8639ZKHcFZd0vSQeK54EMSYQ
+TOKEN = "8756448611:AAHbnOlBbZP8639ZKHcFZd0vSQeK54EMSYQ"
 
 bot = telebot.TeleBot(TOKEN)
 
