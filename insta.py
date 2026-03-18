81de8015050b8fcf84e02a41065e9cd51a982fd5
kingofall098 <joewad@indogmail.com>
2026-03-18 20:33:24 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -203,7 +203,7 @@ def dynamic_scrape(url):
         
 
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=True)
 
             context = browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
